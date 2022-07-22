e006ac24b9d4c4787a305d34988362bf213f55d1
Tanay Shukla <53508296+tshukla2001@users.noreply.github.com>
2022-07-22 22:20:16 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
+let bitcoin;
+let ethereum;
+let tether;
+let usdCoin;
+let binanceCoin;
+let binanceUSD;
+
 const options = {
   method: "GET",
   headers: {
@@ -11,14 +18,30 @@ fetch(
   options
 )
   .then((response) => response.json())
-  .then(
-    (response) =>
-      {
-		document.getElementById("btcprice").innerHTML = "$ " + response[0].current_price.toFixed(3);
-		document.getElementById("ethprice").innerHTML = "$ " + response[1].current_price.toFixed(3);
-		document.getElementById("usdtprice").innerHTML = "$ " + response[2].current_price.toFixed(3);
-		document.getElementById("usdprice").innerHTML = "$ " + response[3].current_price.toFixed(3);
-	  }
+  .then((response) => {
+    document.getElementById("btcprice").innerHTML =
+      "$ " + response[0].current_price.toFixed(3);
+    document.getElementById("ethprice").innerHTML =
+      "$ " + response[1].current_price.toFixed(3);
+    document.getElementById("usdtprice").innerHTML =
+      "$ " + response[2].current_price.toFixed(3);
+    document.getElementById("usdprice").innerHTML =
+      "$ " + response[3].current_price.toFixed(3);
 
-  )
+    bitcoin = response[0].current_price.toInteger();
+    ethereum = response[1].current_price.toInteger();
+    tether = response[2].current_price.toInteger();
+    usdCoin = response[3].current_price.toInteger();
+    binanceCoin = response[4].current_price.toInteger();
+    binanceUSD = response[5].current_price.toInteger();
+  })
   .catch((err) => console.error(err));
+
+export default cryptoPrice = [
+  bitcoin,
+  ethereum,
+  tether,
+  usdCoin,
+  binanceCoin,
+  binanceUSD
+]
